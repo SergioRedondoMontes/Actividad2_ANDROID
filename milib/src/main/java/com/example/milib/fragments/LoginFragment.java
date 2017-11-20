8caac87f7c506f0a85b092f35b3f74ffcbadd607
Sergio Redondo Montes <sergio.redondo@live.u-tad.com>
2017-11-20 19:51:54 +0100

    commit root(Empezando con la parte de login)

---
+++ milib/src/main/java/com/example/milib/fragments/LoginFragment.java
@@ -0,0 +1,78 @@
+package com.example.milib.fragments;
+
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.milib.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LoginFragment extends Fragment {
+
+    Button btnRegister,btnLogin;
+    LoginFragmentEvents events;
+    public LoginFragmentListener listener;
+
+    public void setListener(LoginFragmentListener listener) {
+        this.listener = listener;
+    }
+
+    public LoginFragment() {
+        // Required empty public constructor
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+        btnRegister =(Button) v.findViewById(R.id.btnRegister);
+        btnLogin = (Button)v.findViewById(R.id.btnLogin);
+        events=new LoginFragmentEvents(this);
+        btnRegister.setOnClickListener(events);
+        btnLogin.setOnClickListener(events);
+
+        return v;
+    }
+
+}
+
+class LoginFragmentEvents implements View.OnClickListener{
+
+    LoginFragment loginFragment;
+
+    public LoginFragmentEvents(LoginFragment loginFragment){
+        this.loginFragment=loginFragment;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btnRegister){
+            if(this.loginFragment.listener!=null)
+                this.loginFragment.listener.OnRegisteredClicked();
+        }
+        else if(view.getId() == R.id.btnLogin){
+            if(this.loginFragment.listener!=null)
+                this.loginFragment.listener.OnLoginClicked();
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ milib/src/main/java/com/example/milib/fragments/LoginFragmentListener.java
@@ -0,0 +1,11 @@
+package com.example.milib.fragments;
+
+/**
+ * Created by sergioredondo on 20/11/17.
+ */
+
+public interface LoginFragmentListener {
+
+    public void OnRegisteredClicked();
+    public void OnLoginClicked();
+}
