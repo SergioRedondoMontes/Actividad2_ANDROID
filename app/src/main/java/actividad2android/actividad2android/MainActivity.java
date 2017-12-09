d091f146f507f129f8b59f9f19d7c73ac5c3a210
Sergio Redondo Montes <sergio.redondo@live.u-tad.com>
2017-12-09 18:31:38 +0100

    final actividad 3 Para pruebas descomentar la linea 46 para no tener que meter correo y pass

--- app/src/main/java/actividad2android/actividad2android/MainActivity.java
+++ app/src/main/java/actividad2android/actividad2android/MainActivity.java
@@ -42,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
         DataHolder.instance.fireBaseAdmin=new FireBaseAdmin();
         DataHolder.instance.fireBaseAdmin.setListener(mainActivityEvents);
 
-        DataHolder.instance.fireBaseAdmin.login("a@a.com","12345678",this);
+        //descomentar para entrar automaticamente
+       // DataHolder.instance.fireBaseAdmin.login("a@a.com","12345678",this);
     }
 }
 class MainActivityEvents implements LoginFragmentListener,RegisterFragmentListener,FireBaseAdmin.FireBaseAdminListener{
